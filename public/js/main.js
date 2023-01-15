97cf091a9939ab9045587612418bdd55d19cfe19
BellSohn <mcb_box@yahoo.com>
2023-01-15 13:17:17 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,19 @@
 var url = "http://bookingmanagement.com.devel/";
 
-window.addEventListener('load',function(){
-   // alert("pagina bien cargada");
-   
-  
-       $('#client_id').change(function(){
-           //alert("cambio");
+window.addEventListener('load',function(){  
+     
+       $('#client_id').change(function(){   
            var id_cliente = $(this).val();
            $.ajax({
                url:url+'reserva/search/'+id_cliente,
                type:'GET',
                success:function(response){
                    
-                   if(response.cliente){
-                       console.log("existe usuario");
+                   if(response.cliente){                       
                        $('#client_warning').css('display','none');
                        $('#name').val(response.cliente.name);
                        $('#surname').val(response.cliente.surname);
-                   }else{
-                       //console.log("no existe usuario");                       
+                   }else{                       
                        $('#client_warning').css('display','block');
                        $('#client_warning').css('border','2px solid red');
                        
